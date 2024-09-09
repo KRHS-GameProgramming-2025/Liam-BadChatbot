f08b81561de1febb2173ca428324d2373b7ada62
LiamMartlew <liam.martlew@kearsarge.org>
2024-09-09 12:21:03 -0400

    setup

---
+++ hello.py
@@ -0,0 +1,69 @@
+name = input("Greetings, mortal. What's your name? ")
+greeting = "Hello,"
+print(greeting + " " + name)
+
+
+food = input("Whats your favorite food? ")
+if food == "Pizza":
+    print ("I like pizza too. ")
+if food == "pizza":
+    print ("I like pizza too. ")
+    answer = input("Do you like pineapple on it? ")
+    if answer == "Yes":
+        print("Bro thats nasty.")
+    if answer == "yes":
+        print("Bro thats nasty.")
+    elif answer == "No":
+        print ("I don't like it either tbh")
+    elif answer == "no":
+        print ("I don't like it either tbh")
+
+elif food == "Pasta":
+    print("I eata da pasta")
+elif food == "pasta":
+    print("I eata da pasta")
+    
+elif food == "Spaghetti":
+    print("Mamma mia!")
+elif food == "spaghetti":
+    print("Mamma mia!")
+
+
+reply = input("Anyway, what kinda music do you listen to? ")
+if reply == "Rap":
+    print ("Zzzzz. Ur not ganster lil bro")
+if reply == "rap":
+    print ("Zzzzz. Ur not ganster lil bro")
+
+if reply == "Pop":
+    print ("Bruh")
+if reply == "pop":
+    print ("Bruh")
+
+if reply == "Kpop":
+    print ("Get outta here lil bro")
+if reply == "kpop":
+    print ("Get outta here lil bro")
+
+
+reply2 = input ("Changing topics again, what games do you play? ")
+if reply2 == "Minecraft":
+    print ("Pretty basic answer, good game though")
+if reply2 == "minecraft":
+    print ("Pretty basic answer, good game though")
+    
+if reply2 == "Fortnite":
+    print ("In the big 2024? Wild.")
+if reply2 == "fortnite":
+    print ("In the big 2024? Wild.")
+    
+if reply2 == "Rdr2":
+    print ("Based")
+if reply2 == "rdr2":
+    print ("Based")
+
+if reply2 == "Call Of Duty":
+    print ("Pls go outside")
+if reply2 == "call of duty":
+    print ("Pls go outside")
+
